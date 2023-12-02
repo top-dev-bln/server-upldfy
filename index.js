c372fc76c092a9e9964deb0ac00698015508566f
Dev <recoltare.unu@gmail.com>
2023-12-02 13:19:38 +0200

    server-upldfy

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-// Import packages
+const google = require("googleapis").google;
 const express = require("express");
+const cors = require("cors");
 
-// Middlewares
 const app = express();
-app.use(express.json());
+app.use(cors());
 
-// connection
-const port = process.env.PORT || 9001;
-app.listen(port, () => console.log(`Listening to port ${port}`));
+//TODO: import .env
+const CLIENT_ID =
+  "397112946172-11uf7if7onqpi3b0oefdfolci9kph7hj.apps.googleusercontent.com";
+const CLIENT_SECRET = "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";
+const REDIRECT_URI = "https://isolated.vercel.app/";
+
+const oauth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI
+);
+
+app.get("/griveAuth", async (req, res) => {
+  const scopes = ["https://www.googleapis.com/auth/drive"];
+
+  const url = oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: scopes,
+  });
+
+  res.send(url);
+  console.log("got called");
+});
+
+app.listen(3000, () => console.log("Server ready"));
