f72c77d0f4f2ec0ca3c109cabd8549b8c7de9fc4
Dev <recoltare.unu@gmail.com>
2023-12-02 20:04:06 +0200

    environment

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const google = require("googleapis").google;
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = "https://isolated.vercel.app";
 
 const app = express();
 app.use(cors());
 
 //TODO: import .env
-const CLIENT_ID =
-  "397112946172-11uf7if7onqpi3b0oefdfolci9kph7hj.apps.googleusercontent.com";
-const CLIENT_SECRET = "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";
-const REDIRECT_URI = "https://isolated.vercel.app";
 
 const oauth2Client = new google.auth.OAuth2(
   CLIENT_ID,
