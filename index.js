fe982674b9e8c6c539a84e11a6e2d44027af7db9
Dev <recoltare.unu@gmail.com>
2023-12-06 18:56:27 +0200

    Fix token retrieval in /token endpoint

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ app.use(express.json());
 app.post("/token", async (req, res) => {
   const { code, userID } = req.body;
   try {
-    // const { tokens } = await oauth2Client.getToken(code);
-    //const { refresh_token, access_type } = tokens;
-    res.send("sa va fut in gura");
+    const { tokens } = await oauth2Client.getToken(code);
+    const { refresh_token, access_type } = tokens;
+    res.send(refresh_token);
   } catch (error) {
     console.log(error);
     res.send("sa va fut in gura ca nu merge");
