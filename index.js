91175d8da6ea0b118f105e75d7a3837302c6d4f8
Dev <recoltare.unu@gmail.com>
2023-12-06 17:24:29 +0200

    Refactor authentication endpoint and fix typo in route name

--- index.js
+++ index.js
@@ -3,31 +3,28 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const { createClient } = require("@supabase/supabase-js");
+
+dotenv.config();
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const supabaseUrl = process.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
 const REDIRECT_URI = "https://isolated.vercel.app";
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-dotenv.config();
-
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 const oauth2Client = new google.auth.OAuth2(
   CLIENT_ID,
   CLIENT_SECRET,
   REDIRECT_URI
 );
 
-app.post("/token", async (req, res) => {
-  const { code, userID } = req.body;
-  console.log("tokens: " + code);
-  const { tokens } = await oauth2Client.getToken(code);
-  const { refresh_token, access_token } = tokens;
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-  res.send(JSON.stringify({ refresh_token }));
+app.post("/auth", async (req, res) => {
+  const { code, userID } = req.body;
+
+  res.send(code, userID);
 });
 
 app.get("/griveRedirect", async (req, res) => {
