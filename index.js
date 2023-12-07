7c0309cdb43b20d6f0031d03ac95fb094bc2daa1
Dev <recoltare.unu@gmail.com>
2023-12-07 22:52:43 +0200

    sdf

--- index.js
+++ index.js
@@ -22,20 +22,22 @@ app.use(cors());
 app.use(express.json());
 
 app.post("/token", async (req, res) => {
-  const { code } = req.body;
-  try {
-    const { tokens } = await oauth2Client.getToken(code);
-  } catch (error) {
-    console.error("Error retrieving access token", error);
-    res.send(JSON.stringify({ eruare: error }));
-    return;
-  }
-  //const { refresh_token, access_token } = tokens;
-  console.log("vorba aia : ");
-  console.log(code);
-  //console.log(tokens);
+  //const { code } = req.body;
+  console.log(req.body);
+  /*try {
+          const { tokens } = await oauth2Client.getToken(code);
+        } catch (error) {
+          console.error("Error retrieving access token", error);
+          res.send(JSON.stringify({ eruare: error }));
+          return;
+        }
+        //const { refresh_token, access_token } = tokens;
+        console.log("vorba aia : ");
+        console.log(code);
+        //console.log(tokens);
 
-  res.send(JSON.stringify({}));
+        res.send(JSON.stringify({}));*/
+  res.send(JSON.stringify({ cock: "big" }));
 });
 
 app.get("/", async (req, res) => {
